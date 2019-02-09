e865112b8ce3e8066426c68514e1ddac3a62fdb4
Tomasz Babiuk <tomasz.babiuk@intive.com>
2019-02-09 07:47:33 +0100

    storyboard based view router

--- iosApp/iosApp/StackViewController.swift
+++ iosApp/iosApp/StackViewController.swift
@@ -6,16 +6,68 @@
 //
 
 import UIKit
+import main
+
+protocol ViewRouter {
+    func routeToView(factory: KonakisViewModelFactory) throws -> UIViewController
+}
+
+class RoutingException : Error {
+}
+
+class StoryboardViewRouter: ViewRouter {
+    
+    let storyboard: UIStoryboard
+    
+    init(storyboardName:String) {
+        storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+    }
+    
+    func routeToView(factory: KonakisViewModelFactory) throws -> UIViewController {
+        if factory is TermsViewModelFactory {
+            return storyboard.instantiateViewController(withIdentifier: "Terms")
+        }
+        
+        if factory is DemoViewModelFactory {
+            return storyboard.instantiateViewController(withIdentifier: "Demo")
+        }
+        
+        throw RoutingException()
+    }
+}
+
+class iOSNavigationService : NSObject, NavigationService {
+    
+    let navigationController: UINavigationController
+    let viewRouter: ViewRouter
+    
+    init(navigationController: UINavigationController, viewRouter: ViewRouter) {
+        self.navigationController = navigationController
+        self.viewRouter = viewRouter
+    }
+    
+    func showViewModel(factory: KonakisViewModelFactory) {
+        do {
+            let viewController = try viewRouter.routeToView(factory: factory)
+            navigationController.pushViewController(viewController, animated: true)
+        } catch {
+            print("dupa dupa")
+        }
+    }
+    
+    func finish() {
+        exit(0)
+    }
+}
 
 class StackViewController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let storyboard = UIStoryboard(name: "Stack", bundle: nil)
-        let firstViewController = storyboard.instantiateViewController(withIdentifier: "first")
-        pushViewController(firstViewController, animated: true)
-
-        let secondViewController = storyboard.instantiateViewController(withIdentifier: "second")
-        pushViewController(secondViewController, animated: true)
+        let router = StoryboardViewRouter(storyboardName: "Stack")
+        let navigationService = iOSNavigationService(navigationController: self, viewRouter: router)
+        let bootstrap = SampleBootstrap()
+        bootstrap.navigationService = navigationService
+        bootstrap.start()
     }
 }
--- iosApp/iosApp/TermsViewController.swift
+++ iosApp/iosApp/TermsViewController.swift
@@ -8,40 +8,29 @@
 import UIKit
 import main
 
-class iOSNavigationService : NSObject, NavigationService {
-    
-    func showViewModel(factory: KonakisViewModelFactory) {
-        
-    }
-    
-    func finish() {
-        exit(0)
-    }
-}
-
 class TermsViewController: UIViewController {
 
-    var model: TermsViewModel?
-
-    init(viewModel: TermsViewModel) {
-        self.model = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        self.model = nil
-        super.init(coder: coder)
-    }
+//    var model: TermsViewModel?
+//
+//    init(viewModel: TermsViewModel) {
+//        self.model = viewModel
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        self.model = nil
+//        super.init(coder: coder)
+//    }
 
     @IBOutlet weak var lblAccept: UIButton!
     @IBOutlet weak var lblReject: UIButton!
     
     @IBAction func onRejectClicked(_ sender: Any) {
-        model?.onReject()
+//        model?.onReject()
     }
     
     @IBAction func onAcceptClicked(_ sender: Any) {
-        model?.onAccept()
+//        model?.onAccept()
     }
     
     override func didReceiveMemoryWarning() {
@@ -50,13 +39,13 @@ class TermsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        model = createModel()
+//        model = ()
     }
     
-    func createModel() -> TermsViewModel {
-        let navService = iOSNavigationService()
-        let model = TermsViewModel(navigationService: navService)
-        return model
-    }
+//    func createModel() -> TermsViewModel {
+//        let navService = iOSNavigationService()
+//        let model = TermsViewModel(navigationService: navService)
+//        return model
+//    }
 }
 
